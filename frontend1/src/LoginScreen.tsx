d60675ff10f27c54aeb5728d192c2d4116ddc181
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-11-07 23:21:08 -0800

    Create LoginScreen.tsx

---
+++ frontend1/src/LoginScreen.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg' // temp logo
+
+import './App.css'
+
+function LoginScreen() {
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('');
+
+    return(
+        <>
+            <div>
+                <img src={reactLogo} className="logo react" alt="Logo"/>
+
+                <div>
+                    <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username'/>
+                    <br/>
+                    <input value={password} onChange={e => setPassword(e.target.value)} type= "password" placeholder='Password'/>
+                    <br/>
+                    <button>Log In</button>
+                </div>
+                
+                <button>Sign Up</button>
+
+            </div>
+        </>
+    )
+}
+
+
+export default LoginScreen
