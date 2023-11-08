e2a1469caa701409747813d8abc6da176f73d635
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-11-08 00:10:57 -0800

    remove email from sign up screen

--- frontend1/src/SignUpScreen.tsx
+++ frontend1/src/SignUpScreen.tsx
@@ -7,7 +7,6 @@ import './App.css'
 
 function SignUpScreen() {
     const[username,setUsername] = useState('')
-    const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
 
 
@@ -19,8 +18,6 @@ function SignUpScreen() {
                 <h2>CREATE PROFILE</h2>
                 <div>
                     <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username'/>
-                    <br/>
-                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder='Email'/>
                     <br/>
                     <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password'/>
                 </div>
