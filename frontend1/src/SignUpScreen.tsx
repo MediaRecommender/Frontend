b66272b0e5e01ae0824951c7d47f946f80571906
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-11-07 23:34:39 -0800

    Create SignUpScreen.tsx

---
+++ frontend1/src/SignUpScreen.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg' // temp logo
+
+
+import './App.css'
+
+function SignUpScreen() {
+    const[username,setUsername] = useState('')
+    const[email,setEmail] = useState('')
+    const[password,setPassword] = useState('')
+
+
+    return(
+        <>
+            <div>
+                <img src={reactLogo} className="logo react" alt="Logo"/>
+                <br/>
+                <h2>CREATE PROFILE</h2>
+                <div>
+                    <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username'/>
+                    <br/>
+                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder='Email'/>
+                    <br/>
+                    <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password'/>
+                </div>
+
+                <button>Back</button>
+                <button>Continue</button>
+
+            </div>
+        </>
+    )
+}
+
+export default SignUpScreen
