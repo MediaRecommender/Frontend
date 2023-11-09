b20c4cd5e383953843a4bcfbd5c8e0cb2a7674f3
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-11-08 19:53:31 -0800

    add login verification
    add temporary credentials "username1" and "password1" for testing

--- frontend1/src/LoginScreen.tsx
+++ frontend1/src/LoginScreen.tsx
@@ -4,9 +4,14 @@ import reactLogo from './assets/react.svg' // temp logo
 
 import './App.css'
 
+const testUsername = "username1"
+const testPassword = "password1"
+
 function LoginScreen() {
     const[username,setUsername] = useState('');
     const[password,setPassword] = useState('');
+
+    const[loginErrorDisplayed, setLoginErrorDisplayed] = useState(false);
 
     return(
         <>
@@ -19,7 +24,17 @@ function LoginScreen() {
                     <br/>
                     <input value={password} onChange={e => setPassword(e.target.value)} type= "password" placeholder='Password'/>
                     <br/>
-                    <button>Log In</button>
+                    <button onClick={()=>{
+                        // in the future, the condition for this if statement should be connected somehow to the database
+                        if(username == testUsername && password == testPassword) {
+                            // correct username handling here
+                            setLoginErrorDisplayed(false)
+                        }
+                        else {
+                            setLoginErrorDisplayed(true)
+                        }
+                    }}>Log In</button>
+                    <p hidden={!loginErrorDisplayed}>Incorrect username or password!</p>
                 </div>
                 
                 <button>Sign Up</button>
@@ -28,6 +43,5 @@ function LoginScreen() {
         </>
     )
 }
-
 
 export default LoginScreen
