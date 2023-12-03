00065aaf34eea6e22b173a77358b60cb62b63bec
kippy <amachorro02@gmail.com>
2023-12-03 14:26:51 -0800

    added dropdown menu

--- frontend1/src/TopBar.tsx
+++ frontend1/src/TopBar.tsx
@@ -5,8 +5,17 @@ function TopBar() {
   return (
     <div className = "topBar">
       <img src = "Spotify.png" className="logo"></img>
-      <a href="https://twitter.com/home" className="account-name">Name</a>
-    </div>
+      <div className="dropdown">
+        <button className="account-button">Name</button>
+        <div className="dropdown-content">
+        <a href="#">Sign Out</a>
+        <a href="#">Your Playlist</a>
+        <a href="#">Genre Survey</a>
+        <a href="#">Song Survey</a>
+        <a href="#">Delete Account</a>
+        </div>
+      </div>
+</div>
   )
 }
 
