eb9e575797e7b888b9b35abe79ef3c318c8237e2
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-03 01:24:28 -0800

    separate login handler and add http request

--- frontend1/src/LoginScreen.tsx
+++ frontend1/src/LoginScreen.tsx
@@ -5,6 +5,8 @@ import { useNavigate, redirect } from "react-router-dom";
 
 //import './App.css'
 
+const api = "http://ec2-18-191-32-136.us-east-2.compute.amazonaws.com"
+
 const testUsername = "username1"
 const testPassword = "password1"
 
@@ -14,42 +16,76 @@ function LoginScreen() {
 
     const navigate = useNavigate();
 
-    const[username,setUsername] = useState('');
-    const[password,setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const[loginErrorDisplayed, setLoginErrorDisplayed] = useState(false);
-    
+    const [loginErrorDisplayed, setLoginErrorDisplayed] = useState(false);
+
+    async function loginHandler() {
+        // if loginSuccess is true, the login will proceed; otherwise, show an error
+        var loginSuccess = true;
+
+        try {
+            // fetch is the http request
+            const response = await fetch(api + "/login", {
+                method: "POST",
+                // data is to be sent in a simple two-attribute object
+                body: JSON.stringify({
+                    "username": username,
+                    "password": password,
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            });
+
+            const responsejson = await response.json();
+
+            // backend told me the response should be a simple object with two attributes: "success", a boolean, and "message", a string
+            const responseObject = JSON.parse(responsejson)
+
+            loginSuccess = responseObject.success
+            console.log(responseObject.message)
+        }
+        catch(error) {
+            console.error("http request failed");
+            loginSuccess = false;
+        }
+
+//      if (loginSuccess) {
+        // replace above with below once the backend is properly up
+        if (username == testUsername && password == testPassword) {
+            setLoginErrorDisplayed(false)
+            // make login credentials persistent
+            window.localStorage.setItem("username", username)
+            window.localStorage.setItem("password", password)
+            // move to main app page
+            navigate("/app")
+        }
+        else {
+            setLoginErrorDisplayed(true)
+        }
+    }
 
 
-
-    return(
+    return (
         <>
             <div>
-                <img src={reactLogo} className="logo react" alt="Logo"/>
+                <img src={reactLogo} className="logo react" alt="Logo" />
                 <h2>LOG IN</h2>
 
                 <div>
-                    <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username'/>
-                    <br/>
-                    <input value={password} onChange={e => setPassword(e.target.value)} type= "password" placeholder='Password'/>
-                    <br/>
-                    <button onClick={()=>{
-                        // in the future, the condition for this if statement should be connected somehow to the database
-                        if(username == testUsername && password == testPassword) {
-                            // correct username handling here
-                            setLoginErrorDisplayed(false)
-                            window.localStorage.setItem("username", username)
-                            window.localStorage.setItem("password", password)
-                            navigate("/app")
-                        }
-                        else {
-                            setLoginErrorDisplayed(true)
-                        }
+                    <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username' />
+                    <br />
+                    <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password' />
+                    <br />
+                    <button onClick={() => {
+                        loginHandler()
                     }}>Log In</button>
                     <p hidden={!loginErrorDisplayed}>Incorrect username or password!</p>
                 </div>
-                
-                <button onClick={()=>{navigate("signup")}}>Sign Up</button>
+
+                <button onClick={() => { navigate("signup") }}>Sign Up</button>
 
             </div>
         </>
