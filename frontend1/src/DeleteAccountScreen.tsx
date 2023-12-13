35d643cbfe2c12300ac895ce85ba337ee545336c
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-13 15:25:23 -0800

    add http request to deleteaccount screen

--- frontend1/src/DeleteAccountScreen.tsx
+++ frontend1/src/DeleteAccountScreen.tsx
@@ -1,44 +1,72 @@
 import { useState } from 'react'
 import './DeleteAccountScreen.css'
 import TopBar from './TopBar'
+import Spinner from './Spinner'
+import { useNavigate } from 'react-router-dom'
 
-const testUsername = "username1"
-const testPassword = "password1"
-
+const api = "http://ec2-18-191-32-136.us-east-2.compute.amazonaws.com"
 function Delete() {
 
-  const[username,setUsername] = useState('');
-  const[password,setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const[loginErrorDisplayed, setLoginErrorDisplayed] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+
+  const [loginErrorDisplayed, setLoginErrorDisplayed] = useState(false);
+
+  const navigate = useNavigate()
+
+  async function onClickHandler() {
+    if (username == window.localStorage.getItem("username") && password == window.localStorage.getItem("password")) {
+      try {
+        const response = await fetch(api + "/profile/delete", {
+          method: "POST",
+          body: JSON.stringify({
+            "username": window.localStorage.getItem("username")
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+
+        const responseObject = await response.json();
+
+        if(responseObject.success) {
+          setLoginErrorDisplayed(false)
+          window.localStorage.removeItem("username")
+          window.localStorage.removeItem("password")
+          navigate("/")
+        }
+      }
+      catch (error) {
+        console.error("http request failed: " + error);
+      }
+    }
+    else {
+      setLoginErrorDisplayed(true)
+    }
+  }
+
   return (
     <>
-    <div className='panel'>
-          <p>Please enter your login credentials again: </p>
-                    <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username'/>
-                    <br/>
-                    <input value={password} onChange={e => setPassword(e.target.value)} type= "password" placeholder='Password'/>
-                    <br/>
-                    <button onClick={()=>{
-                        // in the future, the condition for this if statement should be connected somehow to the database
-                        if(username == testUsername && password == testPassword) {
-                            // correct username handling here
-                            setLoginErrorDisplayed(false)
-                        }
-                        else {
-                            setLoginErrorDisplayed(true)
-                        }
-                    }}>Delete Account</button>
-                    <p hidden={!loginErrorDisplayed}>Incorrect username or password!</p>
-                    </div>
+      <div className='panel'>
+        <p>Please enter your login credentials again: </p>
+        <input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder='Username' />
+        <br />
+        <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password' />
+        <br />
+        <button onClick={() => 
+          onClickHandler()}>Delete Account</button>
+        <p hidden={!loginErrorDisplayed}>Incorrect username or password!</p>
+      </div>
     </>
-    )
-  }
-  function DeleteAccountScreen() {
-    const [count, setCount] = useState(0)
-    return (
-        <Delete />
-    )
-  }
-  
-  export default DeleteAccountScreen
+  )
+}
+function DeleteAccountScreen() {
+  const [count, setCount] = useState(0)
+  return (
+    <Delete />
+  )
+}
+
+export default DeleteAccountScreen
