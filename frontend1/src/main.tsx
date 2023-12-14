7cca58c650eb8642ac2fa6dd4675a9b532b6d071
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-13 16:40:38 -0800

    fix a broken link

--- frontend1/src/main.tsx
+++ frontend1/src/main.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element:<GenreSurveyScreen/>
       },
       {
-        path:"playlist",
+        path:"",
         element:<PlaylistScreen/>
       },
       {
